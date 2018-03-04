ad7ea206bc5b7ed5b9c57aad9efab6e1b498f2f5
Rafael Römhild <rafael@roemhild.de>
2018-03-04 23:33:23 +0100

    Update readme and pump version

--- setup.py
+++ setup.py
@@ -4,12 +4,14 @@ import sdist_upip
 
 setup(
     name='micropython-ruuvitag',
-    version='0.3.0',
+    version='0.4.0',
     description='RuuviTag BLE Sensor Beacon scanner.',
-    long_description='More documentation is available at https://github.com/rroemhild/micropython-ruuvitag',
+    long_description=open('README.rst').read(),
     url='https://github.com/rroemhild/micropython-ruuvitag',
     author='Rafael Römhild',
     author_email='rafael@roemhild.de',
+    maintainer='Rafael Römhild',
+    maintainer_email='rafael@roemhild.de',
     license='MIT',
     cmdclass={'sdist': sdist_upip.sdist},
     packages=['ruuvitag'],
