b40b0fe200fe4f27c8b23bf96bc430a79d5f13ff
Rafael Römhild <rafael@roemhild.de>
2021-02-15 20:50:31 +0100

    Import unpack and a2b_base64 to global scope

--- ruuvitag/decoder.py
+++ ruuvitag/decoder.py
@@ -1,5 +1,5 @@
-import ustruct
-import ubinascii
+from ustruct import unpack
+from ubinascii import a2b_base64
 
 
 def decode_data_format_2and4(data):
@@ -9,7 +9,7 @@ def decode_data_format_2and4(data):
     if len(data) > 8:
         identifier = data[8]
         data = data[:8]
-    decoded = ubinascii.a2b_base64(data)
+    decoded = a2b_base64(data)
 
     data_format = decoded[0]
     humidity = decoded[1] / 2
@@ -28,13 +28,12 @@ def decode_data_format_3(data):
         temperature -= 128
         temperature = round(0 - temperature, 2)
 
-    pressure = ustruct.unpack("!H", data[6:8])[0] + 50000
+    pressure = unpack("!H", data[6:8])[0] + 50000
 
-    acceleration_x = ustruct.unpack("!h", data[8:10])[0]
-    acceleration_y = ustruct.unpack("!h", data[10:12])[0]
-    acceleration_z = ustruct.unpack("!h", data[12:14])[0]
-
-    battery_voltage = ustruct.unpack("!H", data[14:16])[0]
+    acceleration_x = unpack("!h", data[8:10])[0]
+    acceleration_y = unpack("!h", data[10:12])[0]
+    acceleration_z = unpack("!h", data[12:14])[0]
+    battery_voltage = unpack("!H", data[14:16])[0]
 
     return (
         3,
@@ -50,23 +49,23 @@ def decode_data_format_3(data):
 
 def decode_data_format_5(data):
     """RuuviTag RAW 2 decoder"""
-    temperature = ustruct.unpack("!h", data[3:5])[0] * 0.005
+    temperature = unpack("!h", data[3:5])[0] * 0.005
 
-    humidity = ustruct.unpack("!H", data[5:7])[0] * 0.0025
+    humidity = unpack("!H", data[5:7])[0] * 0.0025
 
-    pressure = ustruct.unpack("!H", data[7:9])[0] + 50000
+    pressure = unpack("!H", data[7:9])[0] + 50000
 
-    acceleration_x = ustruct.unpack("!h", data[9:11])[0]
-    acceleration_y = ustruct.unpack("!h", data[11:13])[0]
-    acceleration_z = ustruct.unpack("!h", data[13:15])[0]
+    acceleration_x = unpack("!h", data[9:11])[0]
+    acceleration_y = unpack("!h", data[11:13])[0]
+    acceleration_z = unpack("!h", data[13:15])[0]
 
-    power_bin = bin(ustruct.unpack("!H", data[15:17])[0])[2:]
+    power_bin = bin(unpack("!H", data[15:17])[0])[2:]
     battery_voltage = int(power_bin[:11], 2) + 1600
     tx_power = int(power_bin[11:], 2) * 2 - 40
 
     movement_counter = data[18]
 
-    measurement_sequence = ustruct.unpack("!H", data[18:20])[0]
+    measurement_sequence = unpack("!H", data[18:20])[0]
 
     return (
         5,
